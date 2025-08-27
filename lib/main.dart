3f38a9edba993f8ea4b19b24c7e1de5420816954
unknown <emavazquez954@gmail.com>
2025-08-27 16:41:48 -0600

    contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:pmsn2025/utils/colors_app.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+   int contador=0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +21,28 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('PMSN2025'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Text('Contador $contador', style: TextStyle(fontSize: 25, fontFamily: 'EA', color: ColorsApp.txtColor),),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.ads_click),
+          onPressed: (){
+            contador++;
+            print(contador);
+            setState(() {
+              
+            });
+          }
         ),
       ),
     );
   }
+
+  miEvento(){}
 }
+
+//variables estaticas definicion
+//metodos anonimos (){}
+// () => mievento ----> sirve para ejecutar solo una instruccion 
+//interpolacion de cadenas 1 elemento $variable
+//interpolacion de cadenas acceso a variable con un dato ${clase.variable}
---
+++ lib/utils/colors_app.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static Color txtColor = Color.fromARGB(255, 146, 87, 120);
+}
