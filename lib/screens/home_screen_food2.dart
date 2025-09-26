8df2d3e0f07cd6531e59b23fc5ee47195f02369a
unknown <emavazquez954@gmail.com>
2025-09-26 05:00:25 -0600

    Segunda pantalla 80/100 completada

--- lib/screens/home_screen_food2.dart
+++ lib/screens/home_screen_food2.dart
@@ -8,15 +8,56 @@ class HomeScreenFood2 extends StatefulWidget {
 }
 
 class _HomeScreenFood2State extends State<HomeScreenFood2> {
+  final List<Map<String, dynamic>> categories = [
+    {"name": "Burger", "icon": "🍔"},
+    {"name": "Hot Dogs", "icon": "🌭"},
+    {"name": "Kabab", "icon": "🥙"},
+    {"name": "Ice-cream", "icon": "🍦"},
+  ];
+
+  final List<Map<String, dynamic>> food_card = [
+    {
+      "name": "Peperoni Pizza",
+      "description": "Delicious pizza of every person",
+      "image": "assets/ball.png",
+      "calories": "88 Calories",
+      "price": "10.00",
+    },
+
+    {
+      "name": "Peperoni Pizza",
+      "description": "Delicious pizza of every person",
+      "image": "assets/ball.png",
+      "calories": "88 Calories",
+      "price": "10.00",
+    },
+
+    {
+      "name": "Peperoni Pizza",
+      "description": "Delicious pizza of every person",
+      "image": "assets/ball.png",
+      "calories": "88 Calories",
+      "price": "10.00",
+    },
+
+    {
+      "name": "Peperoni Pizza",
+      "description": "Delicious pizza of every person",
+      "image": "assets/ball.png",
+      "calories": "88 Calories",
+      "price": "10.00",
+    },
+  ];
+
+  int selectedIndex = 0;
+  int selectedFoodIndex = 0; 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 230, 230, 230),
+      backgroundColor: const Color.fromARGB(255, 230, 230, 230),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 230, 230, 230),
-        iconTheme: IconThemeData(
-          color: Colors.black
-        ),
+        backgroundColor: const Color.fromARGB(255, 230, 230, 230),
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
       drawer: SafeArea(
         top: true,
@@ -61,8 +102,8 @@ class _HomeScreenFood2State extends State<HomeScreenFood2> {
       ),
       body: ListView(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
             child: Text(
               'Order Delicious Meal!',
               style: TextStyle(
@@ -79,12 +120,12 @@ class _HomeScreenFood2State extends State<HomeScreenFood2> {
               children: [
                 Expanded(
                   child: TextField(
-                    style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                    style: const TextStyle(color: Colors.black),
                     decoration: InputDecoration(
                       hintText: 'Restaurants, Food...',
-                      hintStyle: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+                      hintStyle: const TextStyle(color: Colors.black),
                       filled: true,
-                      fillColor: const Color.fromARGB(255, 255, 255, 255),
+                      fillColor: Colors.white,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(8),
                         borderSide: BorderSide.none,
@@ -95,14 +136,13 @@ class _HomeScreenFood2State extends State<HomeScreenFood2> {
                 ),
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(Icons.tune, color: Colors.black, size: 30,),
+                  icon: const Icon(Icons.tune, color: Colors.black, size: 30),
                 ),
               ],
             ),
           ),
-          SizedBox(height: 15,),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
             child: Text(
               'Explore Categories',
               style: TextStyle(
@@ -110,6 +150,212 @@ class _HomeScreenFood2State extends State<HomeScreenFood2> {
                 fontWeight: FontWeight.bold,
                 fontSize: 20,
                 color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(categories.length, (index) {
+                  final isSelected = selectedIndex == index;
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 300),
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      width: 100,
+                      height: 120,
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: isSelected ? Colors.orange : Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                        boxShadow: [
+                          if (isSelected)
+                            BoxShadow(
+                              color: Colors.orange.withOpacity(0.5),
+                              blurRadius: 10,
+                              offset: const Offset(0, 5),
+                            ),
+                        ],
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Center(
+                                  child: Container(
+                                    width: 50,
+                                    height: 50,
+                                    decoration: const BoxDecoration(
+                                      color: Colors.white,
+                                      shape: BoxShape
+                                          .circle, // 👈 lo hace circular
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Center(
+                                  child: Text(
+                                    categories[index]["icon"],
+                                    style: const TextStyle(fontSize: 30),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(top: 60),
+                                  child: Center(
+                                    child: Text(
+                                      categories[index]["name"],
+                                      style: TextStyle(
+                                        color: isSelected
+                                            ? Colors.white
+                                            : Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(food_card.length, (index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFoodIndex = index;
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      width: 180,
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                          20,
+                        ), // esquinas redondeadas
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            blurRadius: 8,
+                            offset: const Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Imagen circular
+                          Center(
+                            child: ClipOval(
+                              child: Image.asset(
+                                food_card[index]["image"], 
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+
+                          // Nombre
+                          Text(
+                            food_card[index]["name"],
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+
+                          // Descripción
+                          Text(
+                            food_card[index]["description"],
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+
+                          // Calorías
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.local_fire_department,
+                                color: Colors.orange,
+                                size: 18,
+                              ),
+                              const SizedBox(width: 4),
+                              Text(
+                                "${food_card[index]["calories"]} Calories",
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.orange,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+
+                          // Precio y botón favorito
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "\$${food_card[index]["price"]}",
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.orange,
+                                  shape: BoxShape.circle,
+                                ),
+                                padding: const EdgeInsets.all(8),
+                                child: const Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.black,
+                                  size: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
               ),
             ),
           ),
